c4500e5e42a17fc91b558b4e08df6d027c0caaa0
benoitTallandier <benoit.tallandier@gmail.com>
2017-05-03 10:16:51 +0200

    coucou

---
+++ .frama-c/frama_c_journal.ml
@@ -0,0 +1,321 @@
+(* Frama-C journal generated at 09:20 the 02/05/2017 *)
+
+exception Unreachable
+exception Exception of string
+
+(* Run the user commands *)
+let run () =
+  File.init_from_cmdline ();
+  Dynamic.Parameter.String.set "" "/home/benoit/Documents/projetMalg/prog1.c";
+  File.init_from_cmdline ();
+  Project.clear
+    ~selection:(State_selection.of_list
+                  [ State.get "Report.print_once";
+                    State.get "Report.print_csv_once";
+                    State.get "Consolidation graph";
+                    State.get "Consolidated_status" ])
+    ~project:(Project.from_unique_name "default")
+    ();
+  Project.clear
+    ~selection:(State_selection.of_list
+                  [ State.get "Report.print_once";
+                    State.get "Report.print_csv_once";
+                    State.get "Consolidation graph";
+                    State.get "Consolidated_status" ])
+    ~project:(Project.from_unique_name "default")
+    ();
+  Dynamic.Parameter.String.set "" "";
+  Dynamic.Parameter.String.set "" "/home/benoit/Documents/projetMalg/prog1.c";
+  File.init_from_cmdline ();
+  Project.clear
+    ~selection:(State_selection.of_list
+                  [ State.get "Report.print_once";
+                    State.get "Report.print_csv_once";
+                    State.get "Consolidation graph";
+                    State.get "Consolidated_status" ])
+    ~project:(Project.from_unique_name "default")
+    ();
+  Project.clear
+    ~selection:(State_selection.of_list
+                  [ State.get "Report.print_once";
+                    State.get "Report.print_csv_once";
+                    State.get "Consolidation graph";
+                    State.get "Consolidated_status" ])
+    ~project:(Project.from_unique_name "default")
+    ();
+  Project.clear
+    ~selection:(State_selection.of_list
+                  [ State.get "Report.print_once";
+                    State.get "Report.print_csv_once";
+                    State.get "Consolidation graph";
+                    State.get "Consolidated_status" ])
+    ~project:(Project.from_unique_name "default")
+    ();
+  Dynamic.Parameter.String.set "" "";
+  Dynamic.Parameter.String.set "" "/home/benoit/Documents/projetMalg/prog1.c";
+  File.init_from_cmdline ();
+  Dynamic.Parameter.String.set "" "";
+  Dynamic.Parameter.String.set "" "/home/benoit/Documents/projetMalg/prog1.c";
+  File.init_from_cmdline ();
+  Project.clear
+    ~selection:(State_selection.of_list
+                  [ State.get "Report.print_once";
+                    State.get "Report.print_csv_once";
+                    State.get "Consolidation graph";
+                    State.get "Consolidated_status" ])
+    ~project:(Project.from_unique_name "default")
+    ();
+  Dynamic.Parameter.String.set "" "";
+  Dynamic.Parameter.String.set "" "/home/benoit/Documents/projetMalg/prog1.c";
+  File.init_from_cmdline ();
+  Project.clear
+    ~selection:(State_selection.of_list
+                  [ State.get "Report.print_once";
+                    State.get "Report.print_csv_once";
+                    State.get "Consolidation graph";
+                    State.get "Consolidated_status" ])
+    ~project:(Project.from_unique_name "default")
+    ();
+  Project.clear
+    ~selection:(State_selection.of_list
+                  [ State.get "Report.print_once";
+                    State.get "Report.print_csv_once";
+                    State.get "Consolidation graph";
+                    State.get "Consolidated_status" ])
+    ~project:(Project.from_unique_name "default")
+    ();
+  Project.clear
+    ~selection:(State_selection.of_list
+                  [ State.get "Report.print_once";
+                    State.get "Report.print_csv_once";
+                    State.get "Consolidation graph";
+                    State.get "Consolidated_status" ])
+    ~project:(Project.from_unique_name "default")
+    ();
+  Dynamic.Parameter.String.set "" "";
+  Dynamic.Parameter.String.set "" "/home/benoit/Documents/projetMalg/prog1.c";
+  File.init_from_cmdline ();
+  Project.clear
+    ~selection:(State_selection.of_list
+                  [ State.get "Report.print_once";
+                    State.get "Report.print_csv_once";
+                    State.get "Consolidation graph";
+                    State.get "Consolidated_status" ])
+    ~project:(Project.from_unique_name "default")
+    ();
+  Project.clear
+    ~selection:(State_selection.of_list
+                  [ State.get "Report.print_once";
+                    State.get "Report.print_csv_once";
+                    State.get "Consolidation graph";
+                    State.get "Consolidated_status" ])
+    ~project:(Project.from_unique_name "default")
+    ();
+  Project.clear
+    ~selection:(State_selection.of_list
+                  [ State.get "Report.print_once";
+                    State.get "Report.print_csv_once";
+                    State.get "Consolidation graph";
+                    State.get "Consolidated_status" ])
+    ~project:(Project.from_unique_name "default")
+    ();
+  Project.clear
+    ~selection:(State_selection.of_list
+                  [ State.get "Report.print_once";
+                    State.get "Report.print_csv_once";
+                    State.get "Consolidation graph";
+                    State.get "Consolidated_status" ])
+    ~project:(Project.from_unique_name "default")
+    ();
+  Dynamic.Parameter.String.set "" "";
+  Dynamic.Parameter.String.set "" "/home/benoit/Documents/projetMalg/prog1.c";
+  File.init_from_cmdline ();
+  Project.clear
+    ~selection:(State_selection.of_list
+                  [ State.get "Report.print_once";
+                    State.get "Report.print_csv_once";
+                    State.get "Consolidation graph";
+                    State.get "Consolidated_status" ])
+    ~project:(Project.from_unique_name "default")
+    ();
+  Project.clear
+    ~selection:(State_selection.of_list
+                  [ State.get "Report.print_once";
+                    State.get "Report.print_csv_once";
+                    State.get "Consolidation graph";
+                    State.get "Consolidated_status" ])
+    ~project:(Project.from_unique_name "default")
+    ();
+  Project.clear
+    ~selection:(State_selection.of_list
+                  [ State.get "Report.print_once";
+                    State.get "Report.print_csv_once";
+                    State.get "Consolidation graph";
+                    State.get "Consolidated_status" ])
+    ~project:(Project.from_unique_name "default")
+    ();
+  Dynamic.Parameter.String.set "" "";
+  Dynamic.Parameter.String.set "" "/home/benoit/Documents/projetMalg/prog1.c";
+  File.init_from_cmdline ();
+  Project.clear
+    ~selection:(State_selection.of_list
+                  [ State.get "Report.print_once";
+                    State.get "Report.print_csv_once";
+                    State.get "Consolidation graph";
+                    State.get "Consolidated_status" ])
+    ~project:(Project.from_unique_name "default")
+    ();
+  Project.clear
+    ~selection:(State_selection.of_list
+                  [ State.get "Report.print_once";
+                    State.get "Report.print_csv_once";
+                    State.get "Consolidation graph";
+                    State.get "Consolidated_status" ])
+    ~project:(Project.from_unique_name "default")
+    ();
+  Project.clear
+    ~selection:(State_selection.of_list
+                  [ State.get "Report.print_once";
+                    State.get "Report.print_csv_once";
+                    State.get "Consolidation graph";
+                    State.get "Consolidated_status" ])
+    ~project:(Project.from_unique_name "default")
+    ();
+  Dynamic.Parameter.String.set "" "";
+  Dynamic.Parameter.String.set "" "/home/benoit/Documents/projetMalg/prog1.c";
+  File.init_from_cmdline ();
+  Project.clear
+    ~selection:(State_selection.of_list
+                  [ State.get "Report.print_once";
+                    State.get "Report.print_csv_once";
+                    State.get "Consolidation graph";
+                    State.get "Consolidated_status" ])
+    ~project:(Project.from_unique_name "default")
+    ();
+  Project.clear
+    ~selection:(State_selection.of_list
+                  [ State.get "Report.print_once";
+                    State.get "Report.print_csv_once";
+                    State.get "Consolidation graph";
+                    State.get "Consolidated_status" ])
+    ~project:(Project.from_unique_name "default")
+    ();
+  Project.clear
+    ~selection:(State_selection.of_list
+                  [ State.get "Report.print_once";
+                    State.get "Report.print_csv_once";
+                    State.get "Consolidation graph";
+                    State.get "Consolidated_status" ])
+    ~project:(Project.from_unique_name "default")
+    ();
+  Dynamic.Parameter.String.set "" "";
+  Dynamic.Parameter.String.set "" "/home/benoit/Documents/projetMalg/prog1.c";
+  File.init_from_cmdline ();
+  Project.clear
+    ~selection:(State_selection.of_list
+                  [ State.get "Report.print_once";
+                    State.get "Report.print_csv_once";
+                    State.get "Consolidation graph";
+                    State.get "Consolidated_status" ])
+    ~project:(Project.from_unique_name "default")
+    ();
+  Project.clear
+    ~selection:(State_selection.of_list
+                  [ State.get "Report.print_once";
+                    State.get "Report.print_csv_once";
+                    State.get "Consolidation graph";
+                    State.get "Consolidated_status" ])
+    ~project:(Project.from_unique_name "default")
+    ();
+  Project.clear
+    ~selection:(State_selection.of_list
+                  [ State.get "Report.print_once";
+                    State.get "Report.print_csv_once";
+                    State.get "Consolidation graph";
+                    State.get "Consolidated_status" ])
+    ~project:(Project.from_unique_name "default")
+    ();
+  Dynamic.Parameter.String.set "" "";
+  Dynamic.Parameter.String.set "" "/home/benoit/Documents/projetMalg/prog1.c";
+  File.init_from_cmdline ();
+  Project.clear
+    ~selection:(State_selection.of_list
+                  [ State.get "Report.print_once";
+                    State.get "Report.print_csv_once";
+                    State.get "Consolidation graph";
+                    State.get "Consolidated_status" ])
+    ~project:(Project.from_unique_name "default")
+    ();
+  Project.clear
+    ~selection:(State_selection.of_list
+                  [ State.get "Report.print_once";
+                    State.get "Report.print_csv_once";
+                    State.get "Consolidation graph";
+                    State.get "Consolidated_status" ])
+    ~project:(Project.from_unique_name "default")
+    ();
+  Project.clear
+    ~selection:(State_selection.of_list
+                  [ State.get "Report.print_once";
+                    State.get "Report.print_csv_once";
+                    State.get "Consolidation graph";
+                    State.get "Consolidated_status" ])
+    ~project:(Project.from_unique_name "default")
+    ();
+  Project.clear
+    ~selection:(State_selection.of_list
+                  [ State.get "Report.print_once";
+                    State.get "Report.print_csv_once";
+                    State.get "Consolidation graph";
+                    State.get "Consolidated_status" ])
+    ~project:(Project.from_unique_name "default")
+    ();
+  Project.clear
+    ~selection:(State_selection.of_list
+                  [ State.get "Report.print_once";
+                    State.get "Report.print_csv_once";
+                    State.get "Consolidation graph";
+                    State.get "Consolidated_status" ])
+    ~project:(Project.from_unique_name "default")
+    ();
+  Project.clear
+    ~selection:(State_selection.of_list
+                  [ State.get "Report.print_once";
+                    State.get "Report.print_csv_once";
+                    State.get "Consolidation graph";
+                    State.get "Consolidated_status" ])
+    ~project:(Project.from_unique_name "default")
+    ();
+  Dynamic.Parameter.String.set "" "";
+  Dynamic.Parameter.String.set "" "/home/benoit/Documents/projetMalg/prog1.c";
+  File.init_from_cmdline ();
+  !Db.Slicing.Project.set_project None;
+  Dynamic.Parameter.String.set "" "";
+  Dynamic.Parameter.String.set "" "/home/benoit/Documents/projetMalg/prog1.c";
+  File.init_from_cmdline ();
+  !Db.Slicing.Project.set_project None;
+  ()
+
+(* Main *)
+let main () =
+  Journal.keep_file "./.frama-c/frama_c_journal.ml";
+  try run ()
+  with
+  | Unreachable -> Kernel.fatal "Journal reaches an assumed dead code" 
+  | Exception s -> Kernel.log "Journal re-raised the exception %S" s
+  | exn ->
+    Kernel.fatal
+      "Journal raised an unexpected exception: %s"
+      (Printexc.to_string exn)
+
+(* Registering *)
+let main : unit -> unit =
+  Dynamic.register
+    ~plugin:"Frama_c_journal.ml"
+    "main"
+    (Datatype.func Datatype.unit Datatype.unit)
+    ~journalize:false
+    main
+
+(* Hooking *)
+let () = Cmdline.run_after_loading_stage main; Cmdline.is_going_to_load ()
--- prog3.c
+++ prog3.c
@@ -1,22 +1,13 @@
 #include<stdio.h>
-/*@
-    ensures \result == x * x * x;
-*/
+
 int p2(int x){
-    int z=0,v=0,w=1,t=3,u=0,u2=0;
-    /*@ loop invariant z == u*u*u;
-        loop assigns z,v,w,t,u; */
+    unsigned int z=0,v=0,w=1,t=3,u=0,u2=0;
     while ( u < x ){
-        // on calcul (u+1)^3 = u^3 + 3u^2 + 3u + 1
         z=z+v+w;
         v=v+t ;
-        //@ assert v==3*u*u;
         t = t+6;
         w=w+3;
-        //@ assert w==3*u+1;
         u=u+1;
-        //@ assert z==u*u*u;
-        printf("z:%d, v:%d, t:%d, w:%d,u:%d, u2:%d\n",z,v,t,w,u,u2);
     }
     return ( z ) ;
 }
